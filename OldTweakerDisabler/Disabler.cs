03a67f5c01765eb9f00a9ad5bdc97047a57d6c80
APintOfGravy <162986485+APintOfGravy@users.noreply.github.com>
2024-03-31 17:32:25 +0100

    Added Disabler
    Added a Disabler feature. This is designed to allow for installing the original version alongside this custom fork - old custom characters require the original to function, but having both installed at the same time will conflict, thus, this renames the TakeAndHoldTweaker.deli file to prevent it from loading. It's not great, but it works.

---
+++ OldTweakerDisabler/Disabler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using BepInEx;
+using BepInEx.Logging;
+using Mono.Cecil;
+
+namespace OldTweakerDisabler
+{
+    public static class Entrypoint
+    {
+        private static readonly ManualLogSource Logger = BepInEx.Logging.Logger.CreateLogSource("Deliter");
+
+        public static IEnumerable<string> TargetDLLs => Enumerable.Empty<string>();
+
+        public static void Patch(AssemblyDefinition asm)
+        {
+            Logger.LogWarning("No DLLs should be patched, but the patch method was called. Assembly: " + asm);
+        }
+
+        public static void Initialize()
+        {
+            string[] directories = Directory.GetDirectories(Paths.PluginPath);
+
+            foreach (string dir in directories)
+            {
+                Logger.LogInfo($"Found directory {dir}");
+                if (dir.Contains("devyndamonster-TakeAndHoldTweaker") && File.Exists(Path.Combine(dir, "TakeAndHoldTweaker.deli")))
+                {
+                    File.Move(Path.Combine(dir, "TakeAndHoldTweaker.deli"), Path.Combine(dir, "TakeAndHoldTweaker.deli.bak"));
+                }
+            }
+        }
+    }
+}
