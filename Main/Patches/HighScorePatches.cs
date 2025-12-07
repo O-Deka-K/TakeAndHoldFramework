bbb425dc95474a637a7414eb1a17a9dc3adc5ad4
O-Deka-K <56600803+O-Deka-K@users.noreply.github.com>
2025-12-06 19:54:10 -0600

    Prevent debug message "Requesting High Score Chart" from being logged

--- Main/Patches/HighScorePatches.cs
+++ Main/Patches/HighScorePatches.cs
@@ -6,21 +6,29 @@ namespace TNHFramework.Patches
 {
     public static class HighScorePatches
     {
+        // The only reason for overriding this function is to get rid of the debug message "Requesting High Score Chart".
+        // The actual request happens in UpdateHighScoreCallbacks(), which is overridden below.
+        [HarmonyPatch(typeof(TNH_ScoreDisplay), "RequestHighScoreChart")]
+        [HarmonyPrefix]
+        public static bool RequestHighScoreChart_Disable(TNH_ScoreDisplay __instance, ref bool ___m_doRequestScoresTop, ref bool ___m_doRequestScoresPlayer)
+        {
+            __instance.ClearGlobalHighScoreDisplay();
+            return false;
+        }
+
         [HarmonyPatch(typeof(TNH_ScoreDisplay), "UpdateHighScoreCallbacks")]
         [HarmonyPrefix]
-        public static bool RequestScores(ref bool ___m_doRequestScoresTop, ref bool ___m_doRequestScoresPlayer)
+        public static void UpdateHighScoreCallbacks_Disable(ref bool ___m_doRequestScoresTop, ref bool ___m_doRequestScoresPlayer)
         {
             // Custom TNH scoreboard is permanently offline, and official scoreboard doesn't support custom characters
             // Local scores still work
             ___m_doRequestScoresTop = false;
             ___m_doRequestScoresPlayer = false;
-
-            return false;
         }
 
         [HarmonyPatch(typeof(TNH_ScoreDisplay), "SubmitScoreAndGoToBoard")]
         [HarmonyPrefix]
-        public static bool PreventScoring(TNH_ScoreDisplay __instance, string ___m_curSequenceID, ref bool ___m_hasCurrentScore, ref int ___m_currentScore, int score)
+        public static bool SubmitScoreAndGoToBoard_PreventScoring(TNH_ScoreDisplay __instance, string ___m_curSequenceID, ref bool ___m_hasCurrentScore, ref int ___m_currentScore, int score)
         {
             TNHFrameworkLogger.Log("Preventing vanilla score submission", TNHFrameworkLogger.LogType.TNH);
 
