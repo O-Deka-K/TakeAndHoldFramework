e4a679dc1673f09695a5cb39422e66f74de20aee
O-Deka-K <56600803+O-Deka-K@users.noreply.github.com>
2025-12-25 16:43:49 -0600

    Adjust logging

--- Main/TemplateLoadersLegacy.cs
+++ Main/TemplateLoadersLegacy.cs
@@ -95,8 +95,6 @@ namespace TNHFramework
                     }
                 }
 
-                TNHFrameworkLogger.Log("Character loaded successfully : " + character.DisplayName, TNHFrameworkLogger.LogType.General);
-
                 LoadedTemplateManager.AddCharacterTemplate(new ObjectTemplates.CustomCharacter(character), thumbnail);
             }
             catch (Exception e)
