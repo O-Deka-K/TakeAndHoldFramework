1101fd402123e8b3437270971016e13838ff7db5
O-Deka-K <56600803+O-Deka-K@users.noreply.github.com>
2026-08-23 18:34:37 -0500

    Add main menu message when the wrong version of the game is started

---
+++ Main/Patches/MainMenuPatches.cs
@@ -0,0 +1,19 @@
+﻿using FistVR;
+using HarmonyLib;
+
+namespace TNHFramework.Patches
+{
+    static class MainMenuPatches
+    {
+        [HarmonyPatch(typeof(MainMenuScreen), "SetVersionText")]
+        [HarmonyPostfix]
+        private static void SetVersionText_Message(MainMenuScreen __instance)
+        {
+            if (GM.Version_UpdateNumber == 119 && GM.Version_PatchNumber > 0 || GM.Version_UpdateNumber > 119)
+            {
+                string text = __instance.VersionNumberText1.text.Split('\n')[0];
+                __instance.VersionNumberText1.text = text + "\nThis version of TNHFramework is for Update 119 (NOT 119p5)\nDo NOT use on Main or Experimental!";
+            }
+        }
+    }
+}
