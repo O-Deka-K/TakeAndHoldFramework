a70c19688e90874b69ecad74024cd3d0daccb69d
O-Deka-K <56600803+O-Deka-K@users.noreply.github.com>
2026-08-23 18:35:01 -0500

    Added main menu message when the wrong version of the game is started

--- Main/TNHFramework.cs
+++ Main/TNHFramework.cs
@@ -15,7 +15,7 @@ using UnityEngine;
 
 namespace TNHFramework
 {
-    [BepInPlugin("h3vr.tnhframework", "TNH Framework", "0.2.4")]
+    [BepInPlugin("h3vr.tnhframework", "TNH Framework", "0.119.0")]
     [BepInDependency(StratumRoot.GUID, StratumRoot.Version)]
     public class TNHFramework : StratumPlugin
     {
@@ -58,28 +58,34 @@ namespace TNHFramework
         public static List<GameObject> SpawnedPanels = [];
         public static Dictionary<EquipmentPoolDef.PoolEntry.PoolEntryType, List<EquipmentPoolDef.PoolEntry>> SpawnedPoolsDictionary = [];
 
+        public void Awake()
+        {
+            if (TNHFrameworkLogger.BepLog == null)
+                TNHFrameworkLogger.Init();
+
+            TNHFrameworkLogger.Log("Initializing TNHFramework", TNHFrameworkLogger.LogType.General);
+            LoadConfigFile();
+        }
+
         /// <summary>
         /// Initialization method
         /// </summary>
         private void Start()
         {
-            if (TNHFrameworkLogger.BepLog == null)
-                TNHFrameworkLogger.Init();
-
-            TNHFrameworkLogger.Log("Initializing TNHFramework B119", TNHFrameworkLogger.LogType.General);
             TNHFrameworkLogger.Log($"H3VR Update {GetBuildNumber()}", TNHFrameworkLogger.LogType.General);
 
-            SetupOutputDirectory();
-            LoadConfigFile();
-            LoadPanelSprites();
+            Harmony harmony = new("h3vr.tnhframework");
 
             if (GM.Version_UpdateNumber == 119 && GM.Version_PatchNumber > 0 || GM.Version_UpdateNumber > 119)
             {
-                TNHFrameworkLogger.LogError("This should NOT be run on H3VR build 119p5 or experimental. Please switch to Main (Beta: None), Alpha, or Update119. Shutting down...");
+                harmony.PatchAll(typeof(MainMenuPatches));
+                TNHFrameworkLogger.LogError("This should only be run on H3VR update119, NOT update119p5 or main. Shutting down...");
                 return;
             }
 
-            Harmony harmony = new("h3vr.tnhframework");
+            SetupOutputDirectory();
+            LoadPanelSprites();
+
             harmony.PatchAll(typeof(ConstructorPatches));
             harmony.PatchAll(typeof(HighScorePatches));
             harmony.PatchAll(typeof(HoldPatches));
